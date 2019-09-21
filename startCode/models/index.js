710e91943494ad45b451babbcb9a30b2a255a5b5
Evan <evancspellman@gmail.com>
2019-09-20 18:01:13 -0700

    jaws part 1

--- startCode/models/index.js
+++ startCode/models/index.js
@@ -8,6 +8,16 @@ var env = process.env.NODE_ENV || "development";
 var config = require(__dirname + "/../config/config.json")[env];
 var db = {};
 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'TacoB3ll1?',
+    database: 'nba_stats'
+  });
+};
 
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
